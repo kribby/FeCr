ece72f306192e29155b642f128b9278cb6fa5042
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-08-12 09:37:55 -0600

    Update to new constructors

--- src/base/FecrApp.C
+++ src/base/FecrApp.C
@@ -10,10 +10,9 @@ InputParameters validParams<FecrApp>()
   return params;
 }
 
-FecrApp::FecrApp(const std::string & name, InputParameters parameters) :
-    MooseApp(name, parameters)
+FecrApp::FecrApp(InputParameters parameters) :
+    MooseApp(parameters)
 {
-  srand(processor_id());
 
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
